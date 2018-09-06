ea93acca84fa23436b7aa9598d942e1ce469826e
Nikita <nikjk495@gmail.com>
2018-09-06 14:21:25 +0300

    Rewrite test with using application context.

--- spring-core/src/test/java/loggers/ConsoleEventLoggerImplTest.java
+++ spring-core/src/test/java/loggers/ConsoleEventLoggerImplTest.java
@@ -5,11 +5,11 @@ import core.loggers.Event;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.text.DateFormat;
-import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -18,27 +18,32 @@ public class ConsoleEventLoggerImplTest {
 
     private static final String MESSAGE = "Test console logger";
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ConfigurableApplicationContext context;
 
     @Before
-    public void setOutContent(){
+    public void setOutContent() {
         System.setOut(new PrintStream(outContent));
+        context = new ClassPathXmlApplicationContext("spring.xml");
     }
 
     @After
-    public void cleanStreams(){
+    public void cleanStreams() {
         System.setOut(null);
+        context.close();
     }
 
     @Test
     public void logEvent() {
-        ConsoleEventLoggerImpl consoleEventLogger = new ConsoleEventLoggerImpl();
-        Event event = new Event(new Date(),DateFormat.getDateInstance());
+
+        ConsoleEventLoggerImpl consoleEventLogger = context.getBean(ConsoleEventLoggerImpl.class);
+        Event event = context.getBean(Event.class);
+
         event.setMsg(MESSAGE);
 
         consoleEventLogger.logEvent(event);
 
         assertTrue(outContent.toString().contains(event.toString()));
 
-        assertEquals(event.toString().trim(),outContent.toString().trim());
+        assertEquals(event.toString().trim(), outContent.toString().trim());
     }
 }
